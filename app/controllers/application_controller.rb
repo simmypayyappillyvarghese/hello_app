a8f035ebb4df0ccff96bced32ae2df0dc754f7aa
SIMMY PAYYAPPILLY VARGHESE <simmyvarghese5@gmail.com>
2016-04-20 07:02:44 +0000

    Changed the hello message with exclamation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-  render text:"U+00A1hola munda!"
+  render text:"	\u{A1} hola munda !"
   end  
 end
